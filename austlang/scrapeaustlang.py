5d777717cdf1ea8a85c30e9d82909b746ac17fa7
glottotopia <sebastian.nordhoff@glottotopia.de>
2019-11-26 12:23:54 +0100

    better treating of whitespace

--- austlang/scrapeaustlang.py
+++ austlang/scrapeaustlang.py
@@ -6,7 +6,10 @@ from lxml.html.soupparser import fromstring
 
 class Record():
     def fulltext(self, el):
-        return ''.join(el.itertext())
+        try:
+            return ''.join(el.itertext())
+        except AttributeError:
+            return ''
 
     def findtext(self, root, querystring):
         try:
@@ -14,29 +17,35 @@ class Record():
         except AttributeError:
             return ''
         
+    def striptext(self, el):
+        try:
+            return el.text.strip()
+        except AttributeError:
+            return ''
+        
     def __init__(self,root):
-        self.names = dict([(x.find('th').text, x.find('.//div/div/div').text) 
+        self.names = dict([(self.striptext(x.find('th')), self.striptext(x.find('.//div/div/div'))) 
                        for x 
                        in root.findall('.//table[@class="field-group-format group_table_name table table-condensed"]/tbody/tr')
                        ])
         
         self.language_comment = self.fulltext(root.find('.//language_comment'))
-        self.references = [li.text for li in root.findall('.//div[@class="field field-name-field-al-references field-type-text-long field-label-above"]/div/div/ul/li')]
+        self.references = [self.striptext(li) for li in root.findall('.//div[@class="field field-name-field-al-references field-type-text-long field-label-above"]/div/div/ul/li')]
         self.status = self.findtext(root, './/div[@class="field field-name-field-al-status field-type-list-text field-label-above"]/div/div')
         self.state = self.findtext(root, './/div[@class="field field-name-field-state-territory field-type-list-text field-label-above"]/div/div')
         self.location_information = self.fulltext(root.find('.//div[@class="field field-name-field-al-location-info field-type-text-long field-label-above"]/div/div'))
         self.maps = [self.fulltext(x) for x in root.findall('.//div[@class="field field-name-field-al-maps-ozbib field-type-text-long field-label-above"]/div/div/ul/li')]
-        self.links = [(a.attrib["href"],a.text) for a in root.findall('.//div[@class="field field-name-field-al-links field-type-link-field field-label-hidden"]/div/div/a')]
+        self.links = [(a.attrib["href"], self.striptext(a)) for a in root.findall('.//div[@class="field field-name-field-al-links field-type-link-field field-label-hidden"]/div/div/a')]
         self.activities = self.findtext(root, './/div[@class="field field-name-field-al-activities field-type-text-long field-label-above"]/div/div')
         self.people = self.findtext(root, './/div[@class="field field-name-field-al-people field-type-text-long field-label-above"]/div/div')
         self.indigenous_organisations = self.findtext(root, './/div[@class="field field-name-field-al-indig-orgs field-type-text-long field-label-above"]/div/div')        
-        self.speaker_numbers =  [(tr.find('td[1]').text, tr.find('td[2]').text, tr.find('td[3]').text) 
+        self.speaker_numbers =  [(self.striptext(tr.find('td[1]')), self.striptext(tr.find('td[2]')), self.striptext(tr.find('td[3]'))) 
                                  for tr 
                                  in root.findall('.//div[@class="field field-name-ds-code-austlang-speaker-data field-type-ds field-label-hidden"]/div/div/div/table/tbody/tr')[1:]
                                  ]
         documentation = [{'type':self.fulltext(tr.find('th')), 
-                          'size': tr.find('td[1]').text, 
-                          'grade': tr.find('td[2]').text} 
+                          'size': self.striptext(tr.find('td[1]')), 
+                          'grade': self.striptext(tr.find('td[2]'))} 
                          for tr 
                          in root.findall('.//div[@class="field field-name-field-al-documentation-table field-type-text-long field-label-hidden"]/div/div/div/table/tbody/tr')[1:]]        
         self.manuscript_note = self.findtext(root, './/div[@class="field field-name-field-al-manuscript-note field-type-text-long field-label-above"]/div/div')
@@ -63,12 +72,8 @@ if __name__ == "__main__":
     for filename in filenames:
         if filename.endswith('py'):
             continue
-        with open(filename) as file_:
-            try:
-                record = Record(fromstring(file_.read()))
-            except AttributeError:
-                print("could not parse", filename)
-                continue
+        with open(filename) as file_: 
+            record = Record(fromstring(file_.read()))
             records[filename] = record.__dict__
     jrecords = json.dumps(records, sort_keys=True, indent=4)
     with open('out.json','w') as out:
